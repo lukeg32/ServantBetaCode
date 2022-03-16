39a417f71560786127ede613f763ab6b9c5e8b6c
lgantar19 <lgantar19>
2022-03-16 21:05:31 +0000

    working pwm code

--- pythonPWM.py
+++ pythonPWM.py
@@ -1,32 +1,36 @@
 
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+ 
+ledPin = 16
 
-PWMPin = 23
+def setup():
+    global pwm
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.output(ledPin, GPIO.LOW)
+    pwm = GPIO.PWM(ledPin, 1000) # Set Frequency to 1 KHz
+    pwm.start(0) # Set the starting Duty Cycle
+     
+def loop():
+    while True:
+        for dc in range(0, 101, 1):
+            pwm.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+        time.sleep(1)
+        for dc in range(100, -1, -1):
+            pwm.ChangeDutyCycle(dc)
+            time.sleep(0.01)
+        time.sleep(1)
+         
+def destroy():
+    pwm.stop()
+    GPIO.output(ledPin, GPIO.LOW)
+    GPIO.cleanup()
 
-GPIO.setwarnings(False)         #disable warnings
-GPIO.setmode(GPIO.BOARD)        #set pin numbering system
-GPIO.setup(PWMPin,GPIO.OUT)
-
-pi_pwm = GPIO.PWM(PWMPin,1000)      #create PWM instance with frequency
-pi_pwm.start(0)             #start PWM of required Duty Cycle 
-pi_pwm.ChangeDutyCycle(10)
-sleep(1000)
-#while True:
-    #for duty in range(70,101,1):
-        #pi_pwm.ChangeDutyCycle(duty) #provide duty cycle in the range 0-100
-        #sleep(0.05)
-    #sleep(0.5)
-                 #
-    #for duty in range(100,70,-1):
-        #pi_pwm.ChangeDutyCycle(duty)
-        ##sleep(0.05)
-    #sleep(0.5)
-                           
-        
-if  __name__ == '__main__':
+if __name__ == '__main__':
     setup()
     try:
         loop()
-    excep KeyboardInterrupt:
+    except KeyboardInterrupt:
         destroy()
