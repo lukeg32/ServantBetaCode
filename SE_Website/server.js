bf92192009298b5e86d46a355c1d22af568b5ec8
Zeathary <97061809+Zeathary@users.noreply.github.com>
2022-04-01 13:53:04 -0700

    Added functionality to kill python sub processes

--- SE_Website/server.js
+++ SE_Website/server.js
@@ -10,6 +10,8 @@ const {signal} = controller;
 app.use(express.json());
 app.use(cors());
 
+var pid;
+
 // Serve public folder as homepage
 app.use(express.static("/home/pi/ServantBetaCode/SE_Website/public"));
 
@@ -52,8 +54,16 @@ app.get('/pythondispense', (req, res) => {
 
 // Get Request to *hopefully* abort the currently running child process
 app.get('/abort', (req, res) => {
-
-      controller.abort();
+      signal   = signal || 'SIGKILL';
+      // Try to stop python script
+      try { 
+            process.kill(pid, signal);
+            console.log("Child Process Terminated");
+       }
+        catch (ex) { 
+              console.log("Child Process cannot be stopped... We are Doomed");
+              console.log(ex);
+        }
       res.send("Child Process Terminated");
 });
 
@@ -65,6 +75,8 @@ app.post('/exchange', (req, res) => {
       const args = ['exchange.py', req.body.size.toString()]
       // spawn new child process to call the python script with argument of well plate size (in this case 6 or 12)
       const python = spawn('python3', args);
+      // Save pid so it can be stopped if needed
+      pid = python.pid;
       // collect data from script
       python.stdout.on('data', function (data) {
       console.log('Pipe data from python script ...');
