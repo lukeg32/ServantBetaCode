e18a9d8835da57b5daf04ec641f9b73bb9ae0f0d
lgantar19 <lgantar19>
2022-04-01 22:12:57 +0100

    bash script to stop motors when not running

--- SE_Website/exchange.py
+++ SE_Website/exchange.py
@@ -10,6 +10,9 @@ import RPi.GPIO as GPIO
 from termcolor import colored
 from octoprint_cli import __version__
 from octoprint_cli.api import api
+import subprocess
+
+
 
 # Motor
 LEFT_MOTOR = 16
@@ -42,93 +45,98 @@ def dispense(speed, delay):
 # might have a major bug
 def destroy():
     pwmRemoval.stop()
-    GPIO.output(LEFT_MOTOR, GPIO.LOW)
-    GPIO.cleanup()
+    GPIO.output(LEFT_MOTOR, GPIO.HIGH)
 
     pwmDispense.stop()
-    GPIO.output(RIGHT_MOTOR, GPIO.LOW)
+    GPIO.output(RIGHT_MOTOR, GPIO.HIGH)
     GPIO.cleanup()
 
 
+def run_program():
+    # Initialize the gcode directory
+    if sys.argv[1] == "6":
+        pathToFiles = "6wellplate/"
+        gcodeFiles = ["startwell1.gcode", "well2.gcode", "well3.gcode", "well4.gcode", "well5.gcode", "well6.gcode", "end.gcode"]
+    elif sys.argv[1] == "12":
+        pathToFiles = "12wellplate/"
+        gcodeFiles = ["startwell1.gcode", "well2.gcode", "well3.gcode", "well4.gcode", "well5.gcode", "well6.gcode", "end.gcode"]
+    else:
+        print("Invalid plate size")
+
+    # Intialize the OctoPrint caller
+    caller = api("http://10.144.13.13:88", "4AA2ACB8B0DF46479FCB03F9FDC17A60", verbose=False)
+
+    # Test the caller connection
+    if not caller.connectionTest():
+        print("cannot reach server")
+    if not caller.authTest():
+        print("API key issues")
+
+    # Connect to the printer
+    time.sleep(1)
+    data = {'command': 'connect', 'Content-Type': 'application/json'}
+    code = caller.post("/api/connection", data)
+    print("asdf")
+
+    if (code == 204):
+        print("Connected")
+    else:
+        print("Failed")
+        print(code)
+    time.sleep(1)
+
+    # Operate printer
+    #initPWM()
+    print("Going to the first well")
+    count = 1
+    for file in gcodeFiles:
+        # Load the gcode file for this leg
+        code = caller.selectFile(pathToFiles + file)
+        print(pathToFiles + file)
+        if code != 204:
+            print("Failed to load file " + str(code))
+            break
+
+        code = caller.printRequests('start')
+        if code != 204:
+            print("Failed to start")
+            break
+
+        # Wait for motion to finish
+        waitingForPrint = True
+        while (waitingForPrint):
+            state = caller.getState()
+            if (state == 'Operational'):
+                waitingForPrint = False
+            else:
+                time.sleep(10)
+        count += 1
+
+        # Remove media
+        print("Make media removal pump go brrrrr")
+        remove(20, 1)
+        print("Media removed!")
+
+        # Dispense media
+        print("Make media dispensing pump go brrrrr")
+        dispense(20, 1)
+        print("New media dispensed")
 
 
-# Initialize the gcode directory
-if sys.argv[1] == "6":
-    pathToFiles = "6wellplate/"
-    gcodeFiles = ["startwell1.gcode", "well2.gcode", "well3.gcode", "well4.gcode", "well5.gcode", "well6.gcode", "end.gcode"]
-elif sys.argv[1] == "12":
-    pathToFiles = "12wellplate/"
-    gcodeFiles = ["startwell1.gcode", "well2.gcode", "well3.gcode", "well4.gcode", "well5.gcode", "well6.gcode", "end.gcode"]
-else:
-    print("Invalid plate size")
 
-# Intialize the OctoPrint caller
-caller = api("http://10.144.13.13:88", "4AA2ACB8B0DF46479FCB03F9FDC17A60", verbose=False)
+        if (count != 7):
+            print("Going to well", count)
 
-# Test the caller connection
-if not caller.connectionTest():
-    print("cannot reach server")
-if not caller.authTest():
-    print("API key issues")
+    # Disconnect from the printer
+    code = caller.post("/api/connection", {"command": "disconnect"})
+    if (code != 204):
+        print("disconnect")
 
-# Connect to the printer
-time.sleep(1)
-data = {'command': 'connect', 'Content-Type': 'application/json'}
-code = caller.post("/api/connection", data)
-print("asdf")
-
-if (code == 204):
-    print("Connected")
-else:
-    print("Failed")
-    print(code)
-time.sleep(1)
-
-# Operate printer
-initPWM()
-print("Going to the first well")
-count = 1
-for file in gcodeFiles:
-    # Load the gcode file for this leg
-    code = caller.selectFile(pathToFiles + file)
-    print(pathToFiles + file)
-    if code != 204:
-        print("Failed to load file " + str(code))
-        break
-
-    code = caller.printRequests('start')
-    if code != 204:
-        print("Failed to start")
-        break
-
-    # Wait for motion to finish
-    waitingForPrint = True
-    while (waitingForPrint):
-        state = caller.getState()
-        if (state == 'Operational'):
-            waitingForPrint = False
-        else:
-            time.sleep(1)
-    count += 1
-
-    # Remove media
-    print("Make media removal pump go brrrrr")
-    remove(20, 1)
-    print("Media removed!")
-
-    # Dispense media
-    print("Make media dispensing pump go brrrrr")
-    dispense(20, 1)
-    print("New media dispensed")
-
-
-
-    if (count != 7):
-        print("Going to well", count)
-
-# Disconnect from the printer
-code = caller.post("/api/connection", {"command": "disconnect"})
-if (code != 204):
-    print("disconnect")
-
-destroy()
+#destroy()
+if __name__ == '__main__':
+    initPWM()
+    try:
+        run_program()
+    except:
+        destroy()
+        subprocess.run(["./bashGPIO.sh"])
